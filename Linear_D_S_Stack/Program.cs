532f91f311568a83c1c9dc12560f810176e626aa
BADAH Bilal <bilalbadah@gmail.com>
2022-11-19 17:16:56 +0100

    Linear D.S Stack Example Of Work

---
+++ Linear_D_S_Stack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Linear_D_S_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack stackTest = new Stack();
+
+            stackTest.Push(1);
+            stackTest.Push(2);
+            stackTest.Push(3);
+            stackTest.Push(4);
+            stackTest.Push(5);
+            stackTest.Push(6);
+            stackTest.Push(7);
+            stackTest.Push(8);
+            stackTest.Push(9);
+
+            while (!stackTest.isEmpty())
+            {
+                Console.WriteLine(stackTest.TopValue());
+                stackTest.Pop();
+
+            }
+        }
+    }
+}
---
+++ Linear_D_S_Stack/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linear_D_S_Stack
+{
+    public class Stack
+    {
+
+
+        int[] arrValues = new int[10];
+
+        int indexTop = 0;
+
+        public void Push(int val)
+        {
+            if(indexTop == 9)
+            return;
+            arrValues[indexTop] = val;
+            indexTop++;
+        }
+
+        public void Pop()
+        {
+            if (!isEmpty()) 
+            indexTop--;
+        }
+
+        public int TopValue()
+        {
+            if (isEmpty()) return 0 ;
+            return arrValues[indexTop-1];
+        }
+
+        public bool isEmpty()
+        {
+            return indexTop == 0;
+        }
+
+
+
+
+    }
+}
